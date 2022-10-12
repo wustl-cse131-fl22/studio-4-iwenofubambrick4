926271e1fafe0c3fcc5d55ccbe3657db2ae54bf5
siwen <siwen@John.dhcp.wustl.edu>
2022-10-12 17:14:53 -0500

    The Four Musketeres

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,25 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.GREEN);
+		double[] x = {0, 0.5, 1};
+		double[] y = {0, 0.6, 0};
+		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledEllipse(0.25, 0.1, 0.05, 0.025);
+		StdDraw.filledEllipse(0.5, 0.1, 0.05, 0.025);
+		StdDraw.filledEllipse(0.75, 0.1, 0.05, 0.025);
 		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.line(0.78, 0.85, 0.78, 0.75);
+		StdDraw.line(0.78, 0.75, 0.85, 0.75);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(0.02);
+		StdDraw.circle(0.8, 0.8, 0.1);
+		StdDraw.line(0.73, 0.73, 0.87, 0.87);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0, 1, 0.3);
+		StdDraw.line(0.06, 0.66, 0.06, 0.5);
 	}
-}
+}  
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,12 +1,13 @@
 package studio4;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
-
+import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 /**
@@ -19,6 +20,59 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		Color col = new Color(red, green, blue);
+		StdDraw.setPenColor(col);
+		boolean isFilled = in.nextBoolean();
+		
+		if (shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if(isFilled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			} else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		else if (shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if(isFilled) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			} else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+		else if (shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double x[] = {x1, x2, x3};
+			double y[] = {y1, y2, y3};
+			
+			if(isFilled) {
+				StdDraw.filledPolygon(x, y);
+			} else {
+				StdDraw.polygon(x, y);
+			}
+		}
+					
+		
 		
 	}
 }
